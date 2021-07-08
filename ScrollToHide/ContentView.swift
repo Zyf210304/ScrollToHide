9839214dadd2263c61774bc6893ac464b1c16716
张亚飞 <672233580@qq.com>
2021-07-08 16:35:20 +0800

    Initial Commit

---
+++ ScrollToHide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollToHide
+//
+//  Created by 张亚飞 on 2021/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollToHide/ScrollToHideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollToHideApp.swift
+//  ScrollToHide
+//
+//  Created by 张亚飞 on 2021/7/8.
+//
+
+import SwiftUI
+
+@main
+struct ScrollToHideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
